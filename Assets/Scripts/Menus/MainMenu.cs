329577e56c1ce1b14c72870f25a50c70ad1a4037
William <william.bertrand94@gmail.com>
2023-07-08 08:48:56 -0600

    feat: music and sfx events

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 using TMPro;
 
 
@@ -15,29 +16,48 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private MenuRole role;
     [SerializeField] private TMP_Text scoreText;
 
+    private bool _musicStarted;
+
     void Start()
     {
         if(role == MenuRole.SCORE && scoreText != null)
         {
             scoreText.text = string.Format("You Caught {0} Humans!", GameStats.score);
         }
+        else if (role == MenuRole.MAIN)
+        {
+            StartCoroutine(PlayMainMenuMusic());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void OnStartPress()
     {
         GameStats.score = 0;
+        AudioManager.Instance.PlayOneShot(AudioEvent.START_GAME);
         SceneManager.LoadScene("HumanTesting");
     }
 
     public void OnMenuPress()
     {
         GameStats.score = 0;
+        AudioManager.Instance.PlayOneShot(AudioEvent.BUTTON_CLICK);
         SceneManager.LoadScene("Menu");
+    }
+
+    private IEnumerator PlayMainMenuMusic()
+    {
+        while (!AudioManager.Instance.hasLoaded)
+            yield return new WaitForSeconds(0.25f);
+        AudioManager.Instance.PlayMusic(MusicType.MENU);
+    }
+
+    public void OnMouseHoverButton()
+    {
+        AudioManager.Instance.PlayOneShot(AudioEvent.BUTTON_HOVER);
     }
 }
