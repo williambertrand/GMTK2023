c3e2e67ba008c02f818c0db2b303932d2385cc9a
William <william.bertrand94@gmail.com>
2023-07-08 07:45:15 -0600

    wip audio

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
